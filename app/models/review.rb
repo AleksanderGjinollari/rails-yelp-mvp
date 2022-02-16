c4a61adc1a2295d5fecb0d1bfbf5979bbc023449
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-02-16 14:30:58 +0100

    added controllers

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true }
+  validates :rating, :inclusion => 0..5
 end
